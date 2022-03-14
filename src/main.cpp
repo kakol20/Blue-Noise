70aa26f487285fa5ed1aeaec96aaeae400a839fa
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2022-03-14 21:20:48 +0000

    Add Random class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+#include "misc/Random.h"
+
+thread_local unsigned int Random::Seed = 1;
+
 int main() {
 	std::cout << "Hello World!\n";
+	std::cout << "Random Int: " << Random::RandInt() << '\n';
 
 	std::system("pause");
 
